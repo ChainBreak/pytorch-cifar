f04e0850d5000b2902b63ce8db7663f9984f687a
Thomas <thomas.james.rowntree@gmail.com>
2019-10-25 17:34:11 +1030

    begin writing experiment

--- main.py
+++ main.py
@@ -24,27 +24,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
@@ -136,6 +116,86 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
+# for epoch in range(start_epoch, start_epoch+200):
+#     train(epoch)
+#     test(epoch)
+
+def build_parameter_groups(p):
+    
+
+
+def run_experiment(p):
+    
+    ### TRANSFORMS ###
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    ### DATA LOADERS ###
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    p["train_loader"] = trainloader
+    p["test_loader"] = testloader
+
+    ### CRITERION ###
+    criterion = nn.CrossEntropyLoss()
+
+    if p["use_param_groups"]
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+
+#create a list of all the model constructors and their arguments
+net_list = [
+    ( VGG               ,('VGG19',) ),
+    ( ResNet18          ,()         ),
+    ( PreActResNet18    ,()         ),
+    ( GoogLeNet         ,()         ),
+    ( DenseNet121       ,()         ),
+    ( ResNeXt29_2x64d   ,()         ),
+    ( MobileNet         ,()         ),
+    ( MobileNetV2       ,()         ),
+    ( DPN92             ,()         ),
+    # ( ShuffleNetG2      ,()         ),
+    ( SENet18           ,()         ),
+    ( ShuffleNetV2      ,(1,)       ),
+    ( EfficientNetB0    ,()         ),
+]
+
+#create a parameter dictionary that holds the params for the experiments
+p = {}
+p["lr"]=0.01
+p["momentum"] = 0.9
+p["weight_decay"] = 5e-4
+p["num_epochs"] = 200
+p["results_dict"] = {}
+
+#for all models
+for net_class, net_args in net_list:
+
+    
+    p["net_name"] = net_class.__name__
+    p["net"] = net_class(*net_args)
+    
+
+    p1 = p.copy()
+    p1["use_param_groups"] = False
+    run_experiment(p1)
+
+    p2 = p.copy()
+    p2["use_param_groups"] = True
+    run_experiment(p2)
+
